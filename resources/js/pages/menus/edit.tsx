0bbe2a939e9405ae54afe88442b8794054104da0
Tom v. Tilburg <tomvantilburg@pm.me>
2025-09-17 10:11:31 +0200

    :wrench: added basic views and methods to the menus CRUD files

---
+++ resources/js/pages/menus/edit.tsx
@@ -0,0 +1,67 @@
+import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
+import AppLayout from '@/layouts/app-layout';
+import ArticlesLayout from '@/layouts/articles/layout';
+import { create } from '@/routes/articles';
+import { type BreadcrumbItem, type SharedData } from '@/types';
+import { Form, Head, usePage, useForm } from '@inertiajs/react';
+import { articles } from '@/routes';
+import { Input } from '@/components/ui/input';
+import InputError from '@/components/input-error';
+import { Label } from '@/components/ui/label';
+import ArticleController from '@/actions/App/Http/Controllers/Articles/ArticleController';
+import { Button } from '@/components/ui/button';
+import { LoaderCircle } from 'lucide-react';
+import { Select, SelectItem, SelectTrigger, SelectContent, SelectValue } from '@/components/ui/select';
+import { Puck } from '@measured/puck';
+import { config } from '../../../../puck.config';
+import '@measured/puck/puck.css';
+import { router } from '@inertiajs/react';
+import MenusLayout from '@/layouts/menus/layout';
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Articles',
+        href: articles().url
+    },
+    {
+        title: 'Create',
+        href: create().url
+    }
+];
+
+const initialData = { root: { props: {} } };
+
+
+export default function Edit({ article }: { article: SharedData }) {
+    const { auth } = usePage<SharedData>().props;
+    const data: any = article.content;
+    const save = (puckData: any, e?: React.FormEvent) => {
+        if (e) e.preventDefault();
+        console.log(puckData);
+        router.put(ArticleController.update(article.id).url, {
+            title: puckData.root?.props?.title ?? '',
+            content: JSON.stringify(puckData),
+            author_id: auth.user.id,
+            status: 'draft'
+        }, {
+            preserveState: false,
+            preserveScroll: true // optional
+        });
+    };
+
+    return (
+        <AppLayout breadcrumbs={breadcrumbs}>
+            <Head title="Article create" />
+            <MenusLayout>
+                <Puck
+                    config={config}
+                    data={data}
+                    onPublish={save}
+                />
+            </MenusLayout>
+
+        </AppLayout>
+    );
+}
+
+
--- routes/menus.php
+++ routes/menus.php
@@ -5,4 +5,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('menus/create', [MenuController::class, 'create'])->name('menus.create');
 Route::get('menus/{article}', [MenuController::class, 'show'])->name('menus.show');
+Route::get('menus/{article}/edit', [MenuController::class, 'edit'])->name('menus.edit');
+Route::get('menus/{article}/update', [MenuController::class, 'update'])->name('menus.update');
 Route::post('menus/store', [MenuController::class, 'store'])->name('menus.store');
