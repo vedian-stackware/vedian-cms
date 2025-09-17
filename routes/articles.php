f4f59436966c7f0d672534ba97f3260a225368ea
Tom v. Tilburg <tomvantilburg@pm.me>
2025-09-17 10:03:19 +0200

    :wrench: added basic datatable overview

--- routes/articles.php
+++ routes/articles.php
@@ -4,5 +4,7 @@ use App\Http\Controllers\Articles\ArticleController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('articles/create', [ArticleController::class, 'create'])->name('articles.create');
+Route::get('articles/{article}/edit', [ArticleController::class, 'edit'])->name('articles.edit');
+Route::put('articles/{article}/update', [ArticleController::class, 'update'])->name('articles.update');
 Route::get('articles/{article}', [ArticleController::class, 'show'])->name('articles.show');
 Route::post('articles/store', [ArticleController::class, 'store'])->name('articles.store');
