b9b76ff2bdf2f486a270e3b3df52f69ad6b01e83
Tom v. Tilburg <tomvantilburg@pm.me>
2025-09-18 21:30:55 +0200

    :fire: Can select and add menu items to the navbar component.

--- resources/js/pages/menus/create.tsx
+++ resources/js/pages/menus/create.tsx
@@ -42,7 +42,6 @@ export default function Create({ pages }: { pages: any }) {
             return [...prev, selectedPage];
         });
     };
-
     const onChangeUrlSelect = (id: string) => {
         selectedPage = pages.find((p: any) => p.id.toString() === id);
     };
@@ -55,6 +54,26 @@ export default function Create({ pages }: { pages: any }) {
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Menus create" />
             <MenusLayout>
+                <nav
+                    className="relative bg-gray-800/50 after:pointer-events-none after:absolute after:inset-x-0 after:bottom-0 after:h-px after:bg-white/10">
+                    <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+                        <div className="relative flex h-16 items-center justify-between">
+                            <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                                <div className="hidden sm:ml-6 sm:block">
+                                    <div className="flex space-x-4">
+                                        {menuData.length > 0 && menuData.map((item) => (
+                                            <Link href={item.href} key={item.id}
+                                                  onClick={(e: any) => (e.preventDefault())}
+                                                  className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">
+                                                {item.title}
+                                            </Link>
+                                        ))}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
                 <div className="flex rounded-lg">
                     <Select onValueChange={onChangeUrlSelect}>
                         <SelectTrigger className="w-[200px] rounded-r-none">
@@ -75,48 +94,6 @@ export default function Create({ pages }: { pages: any }) {
                         <PlusIcon />
                     </Button>
                 </div>
-                <nav
-                    className="relative bg-gray-800/50 after:pointer-events-none after:absolute after:inset-x-0 after:bottom-0 after:h-px after:bg-white/10">
-                    <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-                        <div className="relative flex h-16 items-center justify-between">
-                            {/*<div className="absolute inset-y-0 left-0 flex items-center sm:hidden">*/}
-                            {/*    <button type="button" command="--toggle" commandfor="mobile-menu"*/}
-                            {/*            className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-white/5 hover:text-white focus:outline-2 focus:-outline-offset-1 focus:outline-indigo-500">*/}
-                            {/*        <span className="absolute -inset-0.5"></span>*/}
-                            {/*        <span className="sr-only">Open main menu</span>*/}
-                            {/*        <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5"*/}
-                            {/*             data-slot="icon" aria-hidden="true" className="size-6 in-aria-expanded:hidden">*/}
-                            {/*            <path d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" stroke-linecap="round"*/}
-                            {/*                  stroke-linejoin="round" />*/}
-                            {/*        </svg>*/}
-                            {/*        <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5"*/}
-                            {/*             data-slot="icon" aria-hidden="true"*/}
-                            {/*             className="size-6 not-in-aria-expanded:hidden">*/}
-                            {/*            <path d="M6 18 18 6M6 6l12 12" stroke-linecap="round" stroke-linejoin="round" />*/}
-                            {/*        </svg>*/}
-                            {/*    </button>*/}
-                            {/*</div>*/}
-                            <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                                {/*<div className="flex shrink-0 items-center">*/}
-                                {/*    <img*/}
-                                {/*        src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"*/}
-                                {/*        alt="Your Company" className="h-8 w-auto" />*/}
-                                {/*</div>*/}
-                                <div className="hidden sm:ml-6 sm:block">
-                                    <div className="flex space-x-4">
-                                        {menuData.length > 0 && menuData.map((item) => (
-                                            <Link href={item.href} key={item.id}
-                                                  onClick={(e: any) => (e.preventDefault())}
-                                                  className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">
-                                                {item.title}
-                                            </Link>
-                                        ))}
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </nav>
             </MenusLayout>
 
         </AppLayout>
